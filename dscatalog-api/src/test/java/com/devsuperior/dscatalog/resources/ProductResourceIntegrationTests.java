16c78b208089fb56e30794488d60df4eabf7b3b7
Jean Carlo Ribeiro <jean.ribeiro@valecard.com.br>
2024-01-16 13:54:56 -0300

    Ajustes dscatalog

---
+++ dscatalog-api/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIntegrationTests.java
@@ -0,0 +1,107 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.factory.Factory;
+import com.devsuperior.dscatalog.services.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductResourceIntegrationTests {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ProductService productService;
+
+  Long existingId;
+  Long notExistingId;
+  Long dependentId;
+  Long countTotalProducts;
+  private PageImpl<ProductDTO> page;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @BeforeEach
+  public void setUp() {
+
+    existingId = 1L;
+    notExistingId = 1000L;
+    dependentId = 2L;
+    countTotalProducts = 25L;
+
+    System.out.println("Config OK..");
+  }
+
+  @Test
+  public void findAllPagedShouldReturnSortedPageWhenSortByName() throws Exception {
+    ResultActions result = mockMvc.perform(get("/products?page=0&size=12&sort=name,asc")
+            .accept(MediaType.APPLICATION_JSON));
+
+    result.andExpect(status().isOk());
+    result.andExpect(jsonPath("$.totalElements").value(countTotalProducts));
+    result.andExpect(jsonPath("$.content").exists());
+    result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+    result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+    result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+
+  }
+
+  @Test
+  public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+    ProductDTO productDTO = Factory.createProductDTO();
+
+    Long expectedId = productDTO.getId();
+    String expectedName = productDTO.getName();
+    String expectedDescription = productDTO.getDescription();
+
+    String json = objectMapper.writeValueAsString(productDTO);
+
+    ResultActions result = mockMvc.perform(put("/products/{id}", existingId)
+            .accept(APPLICATION_JSON)
+            .contentType(APPLICATION_JSON)
+            .content(json)
+
+    );
+    result.andExpect(status().isOk());
+    result.andExpect(jsonPath("$.id").value(expectedId));
+    result.andExpect(jsonPath("$.name").value(expectedName));
+    result.andExpect(jsonPath("$.description").value(expectedDescription));
+
+  }
+
+  @Test
+  public void updateShouldReturnProductDTOWhenIdNotExist() throws Exception {
+    ProductDTO productDTO = Factory.createProductDTO();
+
+    String json = objectMapper.writeValueAsString(productDTO);
+    ResultActions result = mockMvc.perform(put("/products/{id}", notExistingId)
+            .accept(APPLICATION_JSON)
+            .contentType(APPLICATION_JSON)
+            .content(json));
+    result.andExpect(status().isNotFound());
+
+  }
+
+
+}
--- dscatalog-api/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIntegrationTests.java
+++ dscatalog-api/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIntegrationTests.java
@@ -3,34 +3,19 @@ package com.devsuperior.dscatalog.services;
 import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
-import com.devsuperior.dscatalog.factory.Factory;
-import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Transactional
@@ -75,18 +60,18 @@ public class ProductServiceIntegrationTests {
   public void findAllPagedShouldReturnPage() {
     Pageable pageable = PageRequest.of(0, 10);
 
-    Page<ProductDTO> allPaged = productService.findAllPaged(pageable);
-    Assertions.assertNotNull(allPaged);
-    Assertions.assertEquals(0, allPaged.getNumber());
-    Assertions.assertEquals(countTotalProducts, allPaged.getTotalElements());
+    Page<ProductDTO> result = productService.findAllPaged(pageable);
+    Assertions.assertNotNull(result);
+    Assertions.assertEquals(0, result.getNumber());
+    Assertions.assertEquals(countTotalProducts, result.getTotalElements());
   }
 
   @Test
   public void findAllPagedShouldReturnEmptyWhenPageDoesNotExist() {
     Pageable pageable = PageRequest.of(50, 10);
 
-    Page<ProductDTO> allPaged = productService.findAllPaged(pageable);
-    Assertions.assertTrue(allPaged.isEmpty());
+    Page<ProductDTO> result = productService.findAllPaged(pageable);
+    Assertions.assertTrue(result.isEmpty());
   }
 
   @Test
@@ -96,34 +81,9 @@ public class ProductServiceIntegrationTests {
     Page<ProductDTO> allPaged = productService.findAllPaged(pageable);
     Assertions.assertFalse(allPaged.isEmpty());
     Assertions.assertEquals("Macbook Pro", allPaged.getContent().get(0).getName());
+    Assertions.assertEquals("PC Gamer", allPaged.getContent().get(1).getName());
+    Assertions.assertEquals("PC Gamer Alfa", allPaged.getContent().get(2).getName());
 
   }
- /*
-  @Test
-  public void findByIdShouldReturnWhenIdExists() {
-    ProductDTO productDTO = productService.findById(existingId);
-
-    Assertions.assertNotNull(productDTO);
-  }
-
-  @Test
-  public void findByIdShouldReturnNotFoundWhenIdNotExists() {
-
-    Assertions.assertThrows(ResourceNotFoundException.class, () -> productService.findById(notExistingId));
-    Mockito.verify(productRepository).findById(notExistingId);
-  }
-
-  @Test
-  public void updateShouldReturnObjectWhenIdExists() {
-
-    ProductDTO productDTO = productService.update(existingId, Factory.createProductDTO());
-    Assertions.assertNotNull(productDTO);
-  }
-
-  @Test
-  public void updateShouldReturnNotFoundWhenIdNotExists() {
-    ProductDTO productDTO = Factory.createProductDTO();
-    Assertions.assertThrows(ResourceNotFoundException.class, () -> productService.update(notExistingId, productDTO));
-  }*/
 
 }
